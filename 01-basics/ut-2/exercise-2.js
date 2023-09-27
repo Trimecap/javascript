073b5fc88aad46df509cbe92b4394af01c2f1c71
Trimecap <124003420+Trimecap@users.noreply.github.com>
2023-09-27 19:59:59 +0200

    Add files via upload

---
+++ 01-basics/ut-2/exercise-2.js
@@ -0,0 +1,14 @@
+
+let adviceAlert = "This is an alert message, click me to move on"
+alert(adviceAlert)
+
+
+
+let adviceAlert2 = "you clicked on " + confirm("Show me in an alert if you clicked on ok or false") 
+
+alert(adviceAlert2)
+
+
+
+let adviceAlert3 = "You typed " + prompt("Escribe algo", "Type here")
+alert(adviceAlert3)
